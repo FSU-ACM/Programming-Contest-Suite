d781634d1ab3625070382c4f63f55837b7a5aa24
ramonicv <pepsidog0@proton.me>
2025-09-22 10:01:47 -0400

    refactored url paths based on updates made to views.py

--- src/contestadmin/urls.py
+++ src/contestadmin/urls.py
@@ -6,8 +6,8 @@ from .utils import contestadmin_auth
 
 urlpatterns = [
     path('', views.dashboard, name='admin_dashboard'),
-    path('dj_tsv/download', login_required((user_passes_test(contestadmin_auth, login_url='/', redirect_field_name=None))(views.DownloadTSVFiles.as_view())), name='download_dj_files'),
-    path('dj_tsv/generate', login_required((user_passes_test(contestadmin_auth, login_url='/', redirect_field_name=None))(views.GenerateDomJudgeTSV.as_view())), name='gen_dj_files'),
+    path('dj_tsv/download', login_required((user_passes_test(contestadmin_auth, login_url='/', redirect_field_name=None))(views.DownloadDJFiles.as_view())), name='download_dj_files'),
+    path('dj_tsv/generate', login_required((user_passes_test(contestadmin_auth, login_url='/', redirect_field_name=None))(views.GenerateDJFiles.as_view())), name='gen_dj_files'),
     path('ec_files/download', login_required((user_passes_test(contestadmin_auth, login_url='/', redirect_field_name=None))(views.DownloadExtraCreditFiles.as_view())), name='download_ec_files'),
     path('ec_files/email_faculty', login_required((user_passes_test(contestadmin_auth, login_url='/', redirect_field_name=None))(views.EmailFaculty.as_view())), name='email_faculty'),
     path('ec_files/generate', login_required((user_passes_test(contestadmin_auth, login_url='/', redirect_field_name=None))(views.GenerateExtraCreditReports.as_view())), name='gen_ec_reports'),
